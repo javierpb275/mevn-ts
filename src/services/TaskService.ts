117a0bf15d9466d160fbf2c26c3ecaef8b38f538
javierpb275 <javierpb275@gmail.com>
2022-01-01 16:51:53 +0100

    created taskdetail component and functions to get a task, update a task a and delete a task

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -7,3 +7,14 @@ export const createTask = async (task: Task): Promise<AxiosResponse> =>
 
 export const getTasks = async (): Promise<AxiosResponse<Task[]>> =>
   await axios.get("/tasks");
+
+export const getTask = async (id: string): Promise<AxiosResponse<Task>> =>
+  await axios.get(`/tasks/${id}`);
+
+export const updateTask = async (
+  id: string,
+  newTask: Task
+): Promise<AxiosResponse<Task>> => await axios.put(`/tasks/${id}`, newTask);
+
+export const deleteTask = async (id: string): Promise<AxiosResponse> =>
+  await axios.delete(`/tasks/${id}`);
